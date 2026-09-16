b52be597e811bf8b4e9630a0c99670dae29cfe13
ModernChess <taiyeebmuhtadi@gmail.com>
2026-09-16 16:36:00 +0600

    Update firebase-service.js

--- firebase-service.js
+++ firebase-service.js
@@ -1,1 +1,12 @@
-dbdhdhdh
+// firebase-service.js - Updated with the new Firebase Realtime Database URL
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, set, onValue, push, remove, update, get, onDisconnect, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+    databaseURL: "https://mchess12333-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, set, onValue, push, remove, update, get, onDisconnect, serverTimestamp };
